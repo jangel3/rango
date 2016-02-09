0fef4ef476ef93425352a5fde76b05b595bdafd5
John Angel <usrnja@angel.workpc.tds.net>
2016-02-09 09:29:37 -0600

    Added about page. Mapped urls.

--- rango/rango_app/views.py
+++ rango/rango_app/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Rango says hello world!<br/><a href='/rango/about/'>about</a>")
+
+
+def about(request):
+    return HttpResponse("Rango Says: Here is the about page.<br/><a href='/rango/'>Home</a>")
