8872c1b9b31d1316c286b324a1c3dc796813e781
John Angel <jangel3@gmail.com>
2016-02-11 09:44:07 -0600

    Added models and data to the database.  Used python file to populate db and loaddata via json file.

--- rango/settings.py
+++ rango/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango',
+    'rango.rango_app',
 )
 
 MIDDLEWARE_CLASSES = (
